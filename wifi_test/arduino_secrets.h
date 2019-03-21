a0278b61e74a132a590c3004e895cddaf4b02d8e
Andrew Friend <mandrewfriend@gmail.com>
2019-03-21 14:44:29 -0400

    Repo clean up and update to WiFi header file

--- wifi_test/arduino_secrets.h
+++ wifi_test/arduino_secrets.h
@@ -1,2 +1,5 @@
-#define SECRET_SSID "MySpectrumWiFi57-2G"
-#define SECRET_PASS "palmwest691"
+//#define SECRET_SSID "MySpectrumWiFi57-2G"
+//#define SECRET_PASS "palmwest691"
+
+#define SECRET_SSID "WiFi@OSU"
+#define SECRET_PASS "Not-Needed"
